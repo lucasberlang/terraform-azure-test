630aee765495d8aaf7fed3d2f0bd454e133057ac
Lucas <lucasberlang@gmail.com>
2021-08-31 13:47:30 +0200

    Added output

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,5 @@
 
-# output "linux_vm_ids_front" {
-#   description = "List of the Linux Virtual Machine IDs"
-#   value       = module.compute_front.ids
-# }
-
-# output "data_disk_ids_front" {
-#   description = "List of the Managed Disk IDs"
-#   value       = module.compute_front.disks
-# }
-
-# output "network_interface_ids_front" {
-#   description = "List of the Network Interface IDs"
-#   value       = module.compute_front.nics
-# }
-
-# output "linux_vm_ids_middle" {
-#   description = "List of the Linux Virtual Machine IDs"
-#   value       = module.compute_middle.ids
-# }
-
-# output "data_disk_ids_middle" {
-#   description = "List of the Managed Disk IDs"
-#   value       = module.compute_middle.disks
-# }
-
-# output "network_interface_ids_middle" {
-#   description = "List of the Network Interface IDs"
-#   value       = module.compute_middle.nics
-# }
+output "id" {
+  description = "The ID of the Resource Group."
+  value       = azurerm_resource_group.example-test.id
+}
